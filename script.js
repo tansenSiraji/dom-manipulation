c1323ef2b4074e03e3216d8abf42108b5394af9e
cis101 <tansenananda@gmail.com>
2019-06-10 09:30:13 +0600

    Task 2

--- script.js
+++ script.js
@@ -6,6 +6,25 @@ console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
 
+//Task 2a
+var textBtn = document.getElementById('addText1')
+textBtn.addEventListener('click', function(event){
+    var task2 = document.getElementById('task2a')
+    var paragraph = document.createElement('p')
+    paragraph.innerText = 'Hello World'
+    task2.appendChild(paragraph)
+})
 
+//Task 2b
+function changeBGColor(event){
+    //console.log(event.target.innerText)
+    var body = document.getElementsByTagName('body')[0]
+    body.style.backgroundColor = event.target.innerText.toLowerCase()
+}
+
+//Task 2c
+function redirectToGoogle(event){
+window.location.assign('https://www.google.com')
+}
 
 /* Task 4 using jQuery */
